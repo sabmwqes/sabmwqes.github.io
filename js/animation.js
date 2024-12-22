3446054f175a36bc49199bd347704566aa3f4add
sabmwqes <91858685+sabmwqes@users.noreply.github.com>
2024-12-23 02:37:46 +0900

    [fix] Fix issue with clearInterval() not stopping the interval properly

--- js/animation.js
+++ js/animation.js
@@ -2,39 +2,36 @@ window.onload = function() { // DOM読み込むまで待つ
     
     const letters = "ABCDEFGHKMNOPRSTXZ";
 
-    const objects = document.querySelectorAll(".srcumble-text");
+    const titles = document.querySelectorAll(".srcumble-text");
     
-    let intervals = Array(objects.length).fill(null);
-    
+    let intervals = Array(titles.length).fill(null);
+    let iterations = Array(titles.length).fill(0);
+
     const stringScrumble = (target, index) => {
-        let iteration = 0;
-
-        // clearInterval(intervals[index]);
-
-        intervals[index] = setInterval(() => {
+        target.iteration = 0; // js object magic
+        target.interval = setInterval(() => {
             target.innerText = target.innerText
             .split("")
-            .map((letter, index) => {
-                if(index < iteration) {
-                return target.dataset.value[index];
-                }
-            
+            .map((letter, i) => {
+                if(i < target.iteration) return target.dataset.value[i];
                 return letters[Math.floor(Math.random() * letters.length)]
             })
             .join("");
             
-            if(iteration >= target.dataset.value.length){
-                clearInterval(intervals);
+            if(target.iteration >= target.dataset.value.length){
+                clearInterval(target.interval);
             }
             
-            iteration += 1 / 5; //文字列が確定していく速さ
+            target.iteration += 1 / 5; //文字列が確定していく速さ
+            console.log(`iteration: ${target.iteration}, length: ${target.dataset.value.length}`);
             }, 30); //スクランブル間隔(ms)
     }
 
 
     //スクロール感知で実行
-    const cb = function(entries, observer) {
+    const callback = function(entries, observer) {
         entries.forEach((entry, index) => {
+        // console.log(`index: ${index}`);
             if(entry.isIntersecting) {
                 stringScrumble(entry.target, index); //文字列スクランブル
                 observer.unobserve(entry.target); //監視の終了
@@ -48,10 +45,10 @@ window.onload = function() { // DOM読み込むまで待つ
         threshold: 0
     }
     // IntersectionObserverインスタンス化
-    const io = new IntersectionObserver(cb, options);
+    const observer = new IntersectionObserver(callback, options);
     // 監視を開始
-    objects.forEach(object => {
-        io.observe(object);
+    titles.forEach(title => {
+        observer.observe(title);
     });
 
     // DISCLAIMER: DON'T ASK ME ABOUT THIS CODE.
